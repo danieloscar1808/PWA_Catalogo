2cddf70b6b98f4b636efb20147286e2b130439b7
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-09 12:06:06 -0300

    Implement service worker install and fetch handlers
    Add service worker caching for installation and fetch events.

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch',()=>{});
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("catalogo-cache").then((cache) =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./styles.css",
+        "./app.js",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./splash.png"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});

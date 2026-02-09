71e8b9112a12182b073c7598436e4aae752ef868
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-09 12:37:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
-self.addEventListener("install", (e) => {
+self.addEventListener("install",e=>{
   e.waitUntil(
-    caches.open("catalogo-cache").then((cache) =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./styles.css",
-        "./app.js",
-        "./manifest.json",
-        "./icon-192.png",
-        "./icon-512.png",
-        "./splash.png"
-      ])
-    )
+    caches.open("catalogo-cache").then(c=>c.addAll([
+      "./",
+      "./index.html",
+      "./styles.css",
+      "./app.js",
+      "./manifest.json",
+      "./icon-192.png",
+      "./icon-512.png",
+      "./splash.png"
+    ]))
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener("fetch",e=>{
   e.respondWith(
-    caches.match(e.request).then((resp) => resp || fetch(e.request))
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
